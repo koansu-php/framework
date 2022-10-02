b6c95bf1f4bec9b530baac6affc8f285bbd6a86c
Michael Tils <mtils@web-utils.de>
2022-10-02 10:21:22 +0200

    Fixes #1 Ported all planned classes from ems

--- src/Core/Map.php
+++ src/Core/Map.php
@@ -6,8 +6,16 @@
 namespace Koansu\Core;
 
 use Closure;
-use Traversable;
+
+use function array_key_exists;
+use function array_pop;
 use function call_user_func;
+use function explode;
+use function is_array;
+use function preg_quote;
+use function preg_split;
+use function strlen;
+use function substr;
 
 class Map
 {
@@ -65,5 +73,154 @@ class Map
             }
             return $results;
         };
+    }
+
+    /**
+     * Get a key from a nested array. Query a deeply nested array with
+     * property.child.name.
+     *
+     * @param array  $nested
+     * @param string $key
+     * @param string $delimiter
+     *
+     * @return mixed
+     **/
+    public static function get(array $nested, string $key, string $delimiter = '.')
+    {
+        if ($key == '*') {
+            return $nested;
+        }
+
+        if ($key == $delimiter) {
+            return self::withoutNested($nested);
+        }
+
+        $delimiterLength = 0-strlen($delimiter);
+        if($endsWithDelimiter = (substr($key, $delimiterLength) === $delimiter)) {
+            $key = substr($key, 0, $delimiterLength);
+        }
+
+
+        if (isset($nested[$key])) {
+            return $endsWithDelimiter ? self::withoutNested($nested[$key]) : $nested[$key];
+        }
+
+        foreach (explode($delimiter, $key) as $segment) {
+            if (!is_array($nested) || !array_key_exists($segment, $nested)) {
+                return null;
+            }
+
+            $nested = $nested[$segment];
+        }
+
+        return $endsWithDelimiter ? self::withoutNested($nested) : $nested;
+    }
+
+    /**
+     * Put a flat array in this method, and it will return a recursively nested
+     * version. Separate the segments by $delimiter
+     *
+     * @param array  $flat
+     * @param string $delimiter
+     *
+     * @return array
+     **/
+    public static function nest(array $flat, string $delimiter = '.') : array
+    {
+        $tree = [];
+
+        foreach ($flat as $key => $val) {
+
+            // Get parent parts and the current leaf
+            $parts = self::splitPath($key, $delimiter);
+            $leafPart = array_pop($parts);
+
+            // Build parent structure
+            $parent = &$tree;
+
+            foreach ($parts as $part) {
+                if (!isset($parent[$part])) {
+                    $parent[$part] = [];
+                } elseif (!is_array($parent[$part])) {
+                    $parent[$part] = [];
+                }
+
+                $parent = &$parent[$part];
+            }
+
+            // Add the final part to the structure
+            if (empty($parent[$leafPart])) {
+                $parent[$leafPart] = $val;
+            }
+        }
+
+        return $tree;
+    }
+
+    /**
+     * Converts a nested array to a flat one.
+     *
+     * @param array  $nested    The nested source array
+     * @param string $delimiter Levels connector
+     *
+     * @return array
+     **/
+    public static function flatten(array $nested, string $delimiter = '.') : array
+    {
+        $result = [];
+        static::flattenArray($result, $nested, $delimiter);
+        return $result;
+    }
+
+    /**
+     * Splits the path into an array.
+     *
+     * @param string $path
+     * @param string $separator (default:.)
+     *
+     * @return array
+     **/
+    public static function splitPath(string $path, string $separator = '.') : array
+    {
+        $regex = '/(?<=\w)('.preg_quote($separator, '/').')(?=\w)/';
+        return preg_split($regex, $path, -1);//, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+    }
+
+    /**
+     * Recursively converts nested array into a flat one with keys preserving.
+     *
+     * @param array  $result    Resulting array
+     * @param array  $array     Source array
+     * @param string $delimiter Levels connector
+     * @param ?string $prefix    Key's prefix
+     **/
+    protected static function flattenArray(array &$result, array $array, string $delimiter = '.', string $prefix = null)
+    {
+        foreach ($array as $key => $value) {
+            if (!is_array($value) || isset($value[0])) {
+                $result[$prefix.$key] = $value;
+                continue;
+            }
+            self::flattenArray($result, $value, $delimiter, $prefix.$key.$delimiter);
+        }
+    }
+
+    /**
+     * @param array $array
+     * @return array
+     */
+    protected static function withoutNested(array $array) : array
+    {
+        $filtered = [];
+        foreach ($array as $key=>$value) {
+            if (!is_array($value)) {
+                $filtered[$key] = $value;
+                continue;
+            }
+            if (isset($value[0])) {
+                $filtered[$key] = $value;
+            }
+        }
+        return $filtered;
     }
 }
--- tests/unit/Core/MapTest.php
+++ tests/unit/Core/MapTest.php
@@ -1,4 +1,5 @@
-<?php
+<?php /** @noinspection SpellCheckingInspection */
+
 /**
  *  * Created by mtils on 17.12.17 at 11:02.
  **/
@@ -65,4 +66,411 @@ class MapTest extends TestCase
         $items = [];
         $this->assertFalse(Map::all($items, 'is_string'));
     }
+
+    /**
+     * @test
+     */
+    public function nest_with_one_level()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            'address.id'    => 578,
+            'address.street'=> 'Elmstreet 13'
+        ];
+
+        $addressArray = [
+            'id'     => 578,
+            'street' => 'Elmstreet 13'
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($addressArray, $nested['address']);
+    }
+
+    /**
+     * @test
+     */
+    public function flatten_with_one_level()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            'address.id'    => 578,
+            'address.street'=> 'Elmstreet 13'
+        ];
+
+        $addressArray = [
+            'id'     => 578,
+            'street' => 'Elmstreet 13'
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($addressArray, $nested['address']);
+        $this->assertEquals($array, Map::flatten($nested));
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_with_one_level_contains_list()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            'address.id'    => 578,
+            'address.street'=> 'Elmstreet 13',
+            'address.lang' => ['en', 'de', 'fr']
+        ];
+
+        $addressArray = [
+            'id'     => 578,
+            'street' => 'Elmstreet 13',
+            'lang'   => ['en', 'de', 'fr']
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($addressArray, $nested['address']);
+    }
+
+    /**
+     * @test
+     **/
+    public function flatten_with_one_level_contains_list()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            'address.id'    => 578,
+            'address.street'=> 'Elmstreet 13',
+            'address.lang' => ['en', 'de', 'fr']
+        ];
+
+        $addressArray = [
+            'id'     => 578,
+            'street' => 'Elmstreet 13',
+            'lang'   => ['en', 'de', 'fr']
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($addressArray, $nested['address']);
+        $this->assertEquals($array, Map::flatten($nested));
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_with_leading_separator_does_not_produce_children()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            '.id'           => 578,
+            'address.street'=> 'Elmstreet 13'
+        ];
+
+        $nested = Map::nest($array);
+
+        $resultArray = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            '.id'           => 578,
+            'address'       => [
+                'street' => 'Elmstreet 13'
+            ]
+        ];
+
+        $this->assertEquals($resultArray, $nested);
+
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_with_trailing_separator_does_not_produce_children()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            'id.'           => 578,
+            'address.street'=> 'Elmstreet 13'
+        ];
+
+        $nested = Map::nest($array);
+
+        $resultArray = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            'id.'           => 578,
+            'address'       => [
+                'street' => 'Elmstreet 13'
+            ]
+        ];
+
+        $this->assertEquals($resultArray, $nested);
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_with_leading_and_trailing_separator_does_not_produce_children()
+    {
+        $array = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            '.id.'          => 578,
+            'address.street'=> 'Elmstreet 13'
+        ];
+
+        $nested = Map::nest($array);
+
+        $resultArray = [
+            'id'            => 13,
+            'name'          => 'Michael',
+            'surname'       => 'Tils',
+            '.id.'          => 578,
+            'address'       => [
+                'street' => 'Elmstreet 13'
+            ]
+        ];
+
+        $this->assertEquals($resultArray, $nested);
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_with_three_levels()
+    {
+        $array = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'category.type.name'    => 'job'
+        ];
+
+        $categoryArray = [
+            'id'     => 27,
+            'name'   => 'worker'
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($categoryArray, $nested['category']['parent']);
+    }
+
+    /**
+     * @test
+     **/
+    public function flatten_with_three_levels()
+    {
+        $array = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'category.type.name'    => 'job'
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($array, Map::flatten($nested));
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_removes_direct_leaf()
+    {
+        $array = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'category.type.name'    => 'job'
+        ];
+
+        $invalidArray = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address'               => 'foo',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'category.type.name'    => 'job'
+        ];
+
+        $nested = Map::nest($array);
+        $nestedFromInvalid = Map::nest($invalidArray);
+
+        $this->assertEquals($nested, $nestedFromInvalid);
+    }
+
+    /**
+     * @test
+     **/
+    public function get_with_separator_returns_root_nodes()
+    {
+        $array = Map::nest([
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'age'                   => 86,
+            'tags'                  => ['one', 'two']
+        ]);
+
+
+        $root = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'age'                   => 86,
+            'tags'                  => ['one', 'two']
+        ];
+
+        $this->assertEquals($root, Map::get($array, '.'));
+
+    }
+
+    /**
+     * @test
+     **/
+    public function get_with_leading_separator_returns_root_of_child()
+    {
+        $array = Map::nest([
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.id'           => 83,
+            'category.name'         => 'delivery',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'age'                   => 86,
+        ]);
+
+        $categoryRoot = [
+            'id'                    => 83,
+            'name'                  => 'delivery'
+        ];
+
+        $this->assertEquals($categoryRoot, Map::get($array, 'category.'));
+    }
+
+    /**
+     * @test
+     **/
+    public function nest_with_different_separator()
+    {
+        $array = [
+            'id'                       => 13,
+            'name'                     => 'Michael',
+            'surname'                  => 'Tils',
+            'address__id'              => 578,
+            'address__street'          => 'Elmstreet 13',
+            'category__parent__id'     => 27,
+            'category__parent__name'   => 'worker',
+            'category__type__name'     => 'job'
+        ];
+
+        $addressArray = [
+            'id'       => 578,
+            'street'   => 'Elmstreet 13'
+        ];
+
+        $categoryArray = [
+            'id'     => 27,
+            'name'   => 'worker'
+        ];
+
+        $typeArray = [
+            'name'     => 'job'
+        ];
+
+        $nested = Map::nest($array, '__');
+
+        $this->assertEquals($addressArray, $nested['address']);
+        $this->assertEquals($categoryArray, $nested['category']['parent']);
+        $this->assertEquals($typeArray, $nested['category']['type']);
+    }
+
+    /**
+     * @test
+     **/
+    public function get_with_three_levels()
+    {
+        $array = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'category.type.name'    => 'job'
+        ];
+
+        $categoryArray = [
+            'id'     => 27,
+            'name'   => 'worker'
+        ];
+
+        $nested = Map::nest($array);
+
+        $this->assertEquals($categoryArray, Map::get($nested, 'category.parent'));
+        $this->assertEquals($array['category.type.name'], Map::get($nested, 'category.type.name'));
+        $this->assertNull(Map::get($nested, 'category.owner.name'));
+    }
+
+    /**
+     * @test
+     **/
+    public function get_with_asterisk_returns_complete_array()
+    {
+        $array = [
+            'id'                    => 13,
+            'name'                  => 'Michael',
+            'surname'               => 'Tils',
+            'address.id'            => 578,
+            'address.street'        => 'Elmstreet 13',
+            'category.parent.id'    => 27,
+            'category.parent.name'  => 'worker',
+            'category.type.name'    => 'job'
+        ];
+
+        $nested = Map::nest($array);
+        $this->assertEquals($nested, Map::get($nested, '*'));
+    }
+
+
 }
