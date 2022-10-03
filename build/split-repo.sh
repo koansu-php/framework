9b5157a306c75514f51638035af7343304fa22eb
Michael Tils <mtils@web-utils.de>
2022-10-03 21:04:05 +0200

    Added first version of subtree split

---
+++ build/split-repo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+CURRENT_BRANCH="main"
+
+function split()
+{
+    SHA1=$(./usr/local/bin/splitsh-lite --prefix="$1")
+    git push "$2" "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add "$1" "$2" || true
+}
+
+git pull origin $CURRENT_BRANCH
+
+remote core git@github.com:koansu-php/core.git
+remote dependency-injection git@github.com:koansu-php/dependency-injection.git
+remote testing git@github.com:koansu-php/testing.git
+
+split 'src/Core' core
+split 'src/DependencyInjection' dependency-injection
+split 'src/Testing' testing
+
